fd96988726fdf37bb71d70d13d41f6f50b6f03be
JessHV <jhanccove@unsa.edu.pe>
2022-04-28 17:25:16 -0500

    Ejercicios/problemas propuestos

---
+++ calculator.java
@@ -0,0 +1,23 @@
+public class Calculadora {
+
+	static int add(int a, int b){ //Antony
+		//return ;
+	}
+
+	static int sub(int a, int b){  //Oliver
+		//return ;
+	}
+	
+	static int mul(int a, int b){ //Jessica
+		//return ;
+	}
+	
+	static int div(int a, int b){  //Josue
+		//return ;
+	}
+	
+	static int mod(int a, int b){  //Carlos
+		//return ;
+	}
+
+}
