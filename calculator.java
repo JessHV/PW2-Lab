ef5b954705bf02de3996a7a5d26ceac89de8d0e9
JessHV <jhanccove@unsa.edu.pe>
2022-04-29 22:07:12 -0500

    Ligeros cambios, comentarios

--- calculator.java
+++ calculator.java
@@ -1,26 +1,28 @@
 public class calculator {
 
     static int add(int a, int b){ //Antony
-	// Devuelve la suma entera de 2 numeros enteros recibidos
-		return a + b ;
+		// Devuelve la suma entera de 2 numeros enteros recibidos
+		return a + b;
 	}
-	// resta 2 numeros enteros
 	static int sub(int a, int b){  //Oliver 
-		return a-b;
+		//Devuelve la resta 2 numeros enteros
+		return a - b;
 	}
 	
 	static int mul(int a, int b){ //Jessica
+		//Devuelve el producto de 2 números enteros
 		return a * b;
 	}
 	
 	static int div(int a, int b){  //Josue
+		//Devuelve la división de 2 números enteros
 		int result;
-		result = a/b;
+		result = a / b;
 		return result;
 	}
 	
 	static int mod(int a, int b){  //Carlos
-		//Devuelve el módulo de 2 enteros
+		//Devuelve el módulo de 2 números enteros
 		return a % b;
 	}
 
